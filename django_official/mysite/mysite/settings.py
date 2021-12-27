11c9f1e173fe2fb664506fadc34df2cbb25a4a05
Devrath Rana <devrath.rana@kelltontech.com>
2021-12-27 23:05:39 +0530

    fix

--- django_official/mysite/mysite/settings.py
+++ django_official/mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ngrane.apps.NgraneConfig',
+    'udemyclone.apps.UdemycloneConfig',
     'todo.apps.TodoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
